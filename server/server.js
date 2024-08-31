13df29266132a987955c2328026de7a9b5dbd9b2
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2024-08-31 09:15:04 +0545

    handling vercel bug

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "https://code-verifier.vercel.app",
+  })
+);
+
 app.post("/api/verify", (req, res) => {
   const { code } = req.body;
 
